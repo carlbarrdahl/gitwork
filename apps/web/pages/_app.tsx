d287f200492d01054c3d012f4080eed7f103f4c7
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-06-28 08:18:41 +0200

    Fix #12 Wallet connect

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,11 +1,30 @@
 import { AppProps } from "next/app";
 import ThemeProvider from "ui";
 
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.rinkeby],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({ appName: "gitwork", chains });
+
+const wagmiClient = createClient({ autoConnect: true, connectors, provider });
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
